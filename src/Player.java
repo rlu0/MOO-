1eb0eb404bb834dd4956e5d6d24cee0fe792515f
rlu0 <rayluo1998@gmail.com>
2016-05-30 12:30:05 -0400

    worked on turning, getters and setters, started on weapons functionality

--- src/Player.java
+++ src/Player.java
@@ -1,17 +1,42 @@
+import java.awt.Rectangle;
+import java.awt.geom.AffineTransform;
 
-public class Player {
+public class Player{
 private int x;
 private int y;
 private int hp;
 private double direction;
-int [] weapons; 
-final int START_HP=666;
+final double START_DIRECTION= 0; 
+int [] weapons= new int[10];
+
+final int BOOT_DAMAGE=34; final Rectangle BOOT_RANGE = new Rectangle(x,y,30,30);
+final int PISTOL_DAMAGE=20; final Rectangle PISTOL_RANGE = new Rectangle(x,y,10,55555);
+
+final int START_HP=100;
 
 Player(int sx, int sy){
 	hp=START_HP;
 	x=sx;
 	y=sy;
+	direction = START_DIRECTION;
+}
 
+void shoot(int i){
+	
+	range(i);
+	dmg(i);
+	
+}
+Rectangle range(int i){
+	AffineTransform transform = new AffineTransform();
+	transform.rotate(direction);
+return BOOT_RANGE;
+}
+
+int dmg(int i){
+	
+	return 100; 
+			
 }
 
 int getX(){
@@ -37,6 +62,27 @@ void addHP(int increment){
 }
 void addWeapon(int type, int ammo){
 	weapons[type]=ammo;
+}
+void turnLeft(){
+	if (direction==0)
+	{
+		direction = 2*Math.PI -.25;
+	}
+	else
+	{
+		direction-=.25;
+	}
+}
+void turnRight(){
+	direction +=10;
+	if (direction==2*Math.PI)
+	{
+		direction =.25;
+	}
+	else
+	{
+		direction+=.25;
+	}
 }
 
 }
