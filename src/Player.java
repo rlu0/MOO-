18735f732eed5dd3188c57b45fff1705a1eb344f
rlu0 <rayluo1998@gmail.com>
2016-05-30 12:47:48 -0400

    moar guns

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,4 @@
+import java.awt.Polygon;
 import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
 
@@ -10,7 +11,14 @@ final double START_DIRECTION= 0;
 int [] weapons= new int[10];
 
 final int BOOT_DAMAGE=34; final Rectangle BOOT_RANGE = new Rectangle(x,y,30,30);
-final int PISTOL_DAMAGE=20; final Rectangle PISTOL_RANGE = new Rectangle(x,y,10,55555);
+final int PISTOL_DAMAGE=10; final Rectangle PISTOL_RANGE = new Rectangle(x,y,10,55555);
+final int SHOTGUN_DAMAGE= 50; final Polygon SHOTGUN_RANGE = new Polygon( new int[]{x,x+50,x-50}, new int[]{y,y+50,y-50},3 ); final int SUPER_SHOTGUN_DMG = 140;
+final int ASSAULT_RIFLE_DMG= 15; final Rectangle ASSAULT_RIFLE_RANGE = new Rectangle(x,y,10,55555);
+final int CHAINGUN_DMG= 20; final Rectangle CHAINGUN_RANGE = new Rectangle(x,y,10,55555);
+final int POTATO_GUN_DMG=1; final Rectangle POTATO_RANGE = new Rectangle(x,y,10,55555);
+final int MANGATGUN_DMG =9999; final Rectangle MANGAT_RANGE = new Rectangle(0,0,55555,55555);
+final int PLASMA_RIFLE_DMG = 25; final Rectangle PLASMA_RANGE = new Rectangle(x,y,20,55555);
+
 
 final int START_HP=100;
 
