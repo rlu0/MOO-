62b1be6d15f8a3db42d84eb026ebd639f9cadaea
sssata <tonywu.ca@gmail.com>
2016-06-02 11:55:39 -0400

    physics code

--- src/TravelTimeWeapons.java
+++ src/TravelTimeWeapons.java
@@ -19,4 +19,85 @@ public class TravelTimeWeapons extends Weapon {
 		super(type, ammo);
 		// TODO Auto-generated constructor stub
 	}
+	
+	private void movePlayer(){
+		
+		while (true) {
+
+			// ACCELERATION
+			p.xSpeed += p.xAccel;
+			p.ySpeed += p.yAccel;
+			p.x += p.xSpeed;
+			p.y += p.ySpeed;
+
+			double constantFriction = 0.05
+					;
+			double quadraticFriction = 0.01;
+
+			// DRAG
+			if (p.xSpeed < 0) {
+				p.xSpeed += (quadraticFriction * p.xSpeed * p.xSpeed);
+				if (p.xSpeed < -constantFriction) {
+					p.xSpeed += constantFriction;
+				} else {
+					p.xSpeed = 0;
+				}
+			}
+			if (p.xSpeed > 0) {
+				p.xSpeed -= (quadraticFriction * p.xSpeed * p.xSpeed);
+				if (p.xSpeed > constantFriction) {
+					p.xSpeed -= constantFriction;
+				} else {
+					p.xSpeed = 0;
+				}
+			}
+
+			if (p.ySpeed < 0) {
+				p.ySpeed += (quadraticFriction * p.ySpeed * p.ySpeed);
+				if (p.ySpeed < -constantFriction) {
+					p.ySpeed += constantFriction;
+				} else {
+					p.ySpeed = 0;
+				}
+			}
+			if (p.ySpeed > 0) {
+				p.ySpeed -= (quadraticFriction * p.ySpeed * p.ySpeed);
+				if (p.ySpeed > constantFriction) {
+					p.ySpeed -= constantFriction;
+				} else {
+					p.ySpeed = 0;
+				}
+			}
+
+			if (p.y < 0) {
+				p.y = 0;
+				p.ySpeed = -p.ySpeed;
+				//p.yAccel = -p.yAccel;
+			}
+			if (p.y > 900 - 10) {
+				p.y = 900 - 10;
+				p.ySpeed = -p.ySpeed;
+				//p.yAccel = -p.yAccel;
+			}
+			if (p.x < 0) {
+				p.x = 0;
+				p.xSpeed = -p.xSpeed;
+				//p.xAccel = -p.xAccel;
+			}
+			if (p.x > 1500 + 10) {
+				p.x = 1500 + 10;
+				p.xSpeed = -p.xSpeed;
+				//p.xAccel = -p.xAccel;
+			}
+
+			try {
+				Thread.sleep(15);
+			} catch (Exception exc) {
+			}
+			frame.repaint();
+		}
+	}
 }
+
+
+
