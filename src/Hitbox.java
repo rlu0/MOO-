67596946ef0153a18bf0db99f897c5c1b0587dc0
sssata <tonywu.ca@gmail.com>
2016-06-16 12:16:40 -0400

    added comments

--- src/Hitbox.java
+++ src/Hitbox.java
@@ -4,110 +4,32 @@ public class Hitbox {
 
 	Hitbox() {
 	}
-
-	/*boolean isIntersect(Hitbox a, Hitbox b) {
-		if (a instanceof Rectangle) {
-			if (b instanceof Rectangle) {
-				return RRIntersect((Rectangle)a,(Rectangle)b);
-			}
-			if (b instanceof Circle) {
-				return RCIntersect((Rectangle)a,(Circle)b);
-			}
-			if (b instanceof Line) {
-				return RLIntersect((Rectangle)a,(Line)b);
-			}
-		}
-		if (a instanceof Circle) {
-			if (b instanceof Rectangle) {
-				return RCIntersect((Rectangle)b, (Circle)a);
-			}
-			if (b instanceof Circle) {
-				return CCIntersect((Circle)a, (Circle)b);
-			}
-			if (b instanceof Line) {
-				return CLIntersect((Circle)a, (Line)b);
-			}
-		}
-		if (a instanceof Line) {
-			if (b instanceof Rectangle) {
-				return RLIntersect((Rectangle)b, (Line)a);
-			}
-			if (b instanceof Circle) {
-				return CLIntersect((Circle)b, (Line)a);
-			}
-			if (b instanceof Line) {
-				return LLIntersect((Line)a, (Line)b);
-			}
-		}
-
-		return false;
-
-	}*/
 	
-	/*boolean isPolygonsIntersecting(Rectangle a, Rectangle b)
-	{
-	    for (int x=0; x<2; x++)
-	    {
-	        Rectangle polygon = (x==0) ? a : b;
-
-	        for (int i1=0; i1<4; i1++)
-	        {
-	            int i2 = (i1 + 1) % 4;
-	            Point p1 = new Point(polygon.getX(), polygon.getY());
-	            Point p2 = new Point(polygon.getX(), polygon.getY());
-
-	            Point normal = new Point(p2.y - p1.y, p1.x - p2.x);
-
-	            double minA = Double.POSITIVE_INFINITY;
-	            double maxA = Double.NEGATIVE_INFINITY;
-
-	            for (Point p : a.getPoints())
-	            {
-	                double projected = normal.x * p.x + normal.y * p.y;
-
-	                if (projected < minA)
-	                    minA = projected;
-	                if (projected > maxA)
-	                    maxA = projected;
-	            }
-
-	            double minB = Double.POSITIVE_INFINITY;
-	            double maxB = Double.NEGATIVE_INFINITY;
-
-	            for (Point p : b.getPoints())
-	            {
-	                double projected = normal.x * p.x + normal.y * p.y;
-
-	                if (projected < minB)
-	                    minB = projected;
-	                if (projected > maxB)
-	                    maxB = projected;
-	            }
-
-	            if (maxA < minB || maxB < minA)
-	                return false;
-	        }
-	    }
-
-	    return true;
-	}*/
-	
+	// Rectangle Rectangle Collision (unused)
 	boolean RRIntersect(RectangleHit a, RectangleHit b){
 		
 		if ((a.getX2()<b.getX1() || b.getX2()<a.getX1()
 				|| a.getY2()<b.getY1() || b.getY2()<a.getY1())){
 			return true;
 		}
-		
 		return false;
-		
 	}
+	
+	
+	/**
+	 * Detects collision between rectangle and circle
+	 * @param a Hitbox of rectangle
+	 * @param b Hitbox of circle
+	 * @return Point of intersection [0]:x [1]:y
+	 */
 	double [] RCIntersect(RectangleHit a, CircleHit b){
 		
 		double [] coord = new double [2];
 		coord[0] = Double.MAX_VALUE;
 		coord[1] = Double.MAX_VALUE;
 		
+		
+		// Detect rectangle edge collisions
 		if (b.getX()>a.getX1() && b.getX()<a.getX2() && b.getY()>a.getY1()-b.getR() && b.getY()<a.getY1()){
 			coord[0] = b.getX();
 			coord[1] = a.getY1();
@@ -129,29 +51,7 @@ public class Hitbox {
 			return coord;
 		}
 
-		
-		
-		/*if (b.getY()>a.getY1() && b.getY()<a.getY2() && b.getX()-b.getR() < a.getX2() && 
-			b.getY()>a.getY1() && b.getY()<a.getY2() && b.getX()+b.getR() > a.getX1() &&
-			b.getX()>a.getX1() && b.getX()<a.getX2() && b.getY()-b.getR() > a.getY1() &&
-			b.getX()>a.getX1() && b.getX()<a.getX2() && b.getY()+b.getR() < a.getY2()){
-			coord[0] = a.getX2();
-			coord[1] = b.getY();
-		}
-		if (b.getY()>a.getY1() && b.getY()<a.getY2() && b.getX()+b.getR() > a.getX1()){
-			coord[0] = a.getX1();
-			coord[1] = b.getY();
-		}
-		
-		if (b.getX()>a.getX1() && b.getX()<a.getX2() && b.getY()-b.getR() > a.getY1()){
-			coord[0] = b.getX();
-			coord[1] = a.getY1();
-		}
-		if (b.getX()>a.getX1() && b.getX()<a.getX2() && b.getY()+b.getR() < a.getY2()){
-			coord[0] = b.getX();
-			coord[1] = a.getY2();
-		}*/
-		
+		// Detect rectangle vertex collisions
 		if (Math.pow(a.getX1()-b.getX(), 2) + Math.pow(a.getY1()-b.getY(), 2)<b.getR()*b.getR()){
 			coord[0] = a.getX1();
 			coord[1] = a.getY1();
@@ -176,20 +76,34 @@ public class Hitbox {
 		return coord;
 	}
 	
-	
+
+	/**
+	 * Finds intersection point of two line segments
+	 * @param x1 x1 of line 1
+	 * @param y1 y1 of line 1
+	 * @param x2 x2 of line 2
+	 * @param y2 y2 of line 2
+	 * @param x3 x1 of line 2
+	 * @param y3 y1 of line 2
+	 * @param x4 x2 of line 2
+	 * @param yy y2 of line 2
+	 * @return Point of intersection [0]:x [1]:y
+	 */
 	double []  lineIntersect(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4) {
 		double [] coord = new double [2];
 		coord [0] = Double.MAX_VALUE;
 		coord [1] = Double.MAX_VALUE;
 		
 		double denom = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
-		if (denom == 0.0) { // Lines are parallel.
+		if (denom == 0.0) {
+			// Lines are parallel
 			return coord;
 		}
+		
 		double ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3))/denom;
 		double ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3))/denom;
 		if (ua >= 0.0f && ua <= 1.0f && ub >= 0.0f && ub <= 1.0f) {
-			// Get the intersection point.
+			// Get the intersection point
 			coord[0] = x1 + ua*(x2 - x1);
 			coord[1] = y1 + ua*(y2 - y1);
 			return coord;
@@ -197,7 +111,12 @@ public class Hitbox {
 		return coord;
 	}
 	
-	
+	/**
+	 * Finds intersection point of a line segment and rectangle
+	 * @param a Hitbox of rectangle
+	 * @param b Hitbox of line
+	 * @return Point of intersection [0]:x [1]:y
+	 */
 	double[] RLIntersect(RectangleHit a, Line b){
 		
 		double [] intersectFinal = new double[2];
@@ -216,109 +135,27 @@ public class Hitbox {
 		
 		// Rectangle Top
 		coord = lineIntersect(b.getX1(), b.getY1(), b.getX2(), b.getY2(), a.getX1(), a.getY1(), a.getX2(), a.getY1());
-		
 		intersectsX[0] = coord[0];
 		intersectsY[0] = coord[1];
 		
-		
 		// Rectangle Bottom
 		coord = lineIntersect(b.getX1(), b.getY1(), b.getX2(), b.getY2(), a.getX1(), a.getY2(), a.getX2(), a.getY2());
-		
 		intersectsX[1] = coord[0];
 		intersectsY[1] = coord[1];
 		
 		// Rectangle  Left
 		coord = lineIntersect(b.getX1(), b.getY1(), b.getX2(), b.getY2(), a.getX1(), a.getY1(), a.getX1(), a.getY2());
-		
 		intersectsX[2] = coord[0];
 		intersectsY[2] = coord[1];
 		
 		// Rectangle  Right
 		coord = lineIntersect(b.getX1(), b.getY1(), b.getX2(), b.getY2(), a.getX2(), a.getY1(), a.getX2(), a.getY2());
-		
 		intersectsX[3] = coord[0];
 		intersectsY[3] = coord[1];
 		
-		/*// Line equation
-		double lineA = b.getY2() - b.getY1();
-		double lineB = b.getX1() - b.getX2();
-		double lineC = lineA * b.getX1() + lineB * b.getY1();
-		
-		
-		//Upper rectangle line
-		double upA = 0;
-		double upB = a.getX1()-a.getX2();
-		//double upC = upB * a.getY1();
-		
-		double det = lineA*upB - upA*lineB;
-		if(det == 0){
-			//Lines are parallel
-		}else{
-			double x = (upB*lineC - lineB*upC)/det;
-			double y = (lineA*upC - upA*lineC)/det;
-			if (x>a.getX1() && x<a.getX2()){
-				intersectsX[0] = x;
-				intersectsY[0] = y;
-			}
-		}*/
-		
-		/*
-		//Down rectangle line
-		double downA = 0;
-		double downB = a.getX1()-a.getX2();
-		double downC = downB * a.getY2();
-		
-		det = lineA*upB - upA*lineB;
-		if(det == 0){
-			//Lines are parallel
-		}else{
-			double x = (downB*lineC - lineB*downC)/det;
-			double y = (lineA*downC - downA*lineC)/det;
-			if (x>a.getX1() && x<a.getX2()){
-				intersectsX[1] = x;
-				intersectsY[1] = y;
-			}
-		}
-		
-		
-		//Left rectangle line
-		double leftA = a.getY2()-a.getY1();
-		double leftB = 0;
-		double leftC = leftA * a.getX1();
-		
-		det = lineA*leftB - leftA*lineB;
-		if(det == 0){
-			//Lines are parallel
-		}else{
-			double x = (leftB*lineC - lineB*leftC)/det;
-			double y = (lineA*leftC - leftA*lineC)/det;
-			if (y>a.getY1() && y<a.getY2()){
-				intersectsX[2] = x;
-				intersectsY[2] = y;
-			}
-		}
-		
-		
-		//right rectangle line
-		double rightA = a.getY2()-a.getY1();
-		double rightB = 0;
-		double rightC = leftA * a.getX2();
-		
-		det = rightA*leftB - rightA*lineB;
-		if(det == 0){
-			//Lines are parallel
-		}else{
-			double x = (rightB*lineC - lineB*rightC)/det;
-			double y = (lineA*rightC - rightA*lineC)/det;
-			if (y>a.getY1() && y<a.getY2()){
-				intersectsX[3] = x;
-				intersectsY[3] = y;
-			}
-		}
-		*/
 		
 		// Find closest intersection point
-		double minDist = Double.MAX_VALUE; // Distance not square rooted
+		double minDist = Double.MAX_VALUE;
 		int minDistIndex = -1;
 		
 		for (int i=0; i<4; i++){
@@ -326,10 +163,9 @@ public class Hitbox {
 			if (intersectsX[i] == Double.MAX_VALUE && intersectsY[i] == Double.MAX_VALUE){
 				continue;
 			}
-			//System.out.println("not break");
+			
 			double currentDist = Math.pow(intersectsX[i]-b.getX1(), 2) + Math.pow(intersectsY[i]-b.getY1(), 2);
 			if (currentDist < minDist){
-				
 				minDist = currentDist;
 				minDistIndex = i;
 			}
@@ -344,7 +180,12 @@ public class Hitbox {
 		
 	}
 	
-	
+	/**
+	 * Finds intersection point of two circles
+	 * @param a Hitbox of circle a
+	 * @param b Hitbox of circle b
+	 * @return Point of intersection [0]:x [1]:y
+	 */
 	double[] CCIntersect(CircleHit a, CircleHit b){
 		
 		double [] intersectFinal = new double[2];
@@ -359,56 +200,56 @@ public class Hitbox {
 		
 	}
 	
-	
-	double dist(double x1, double y1, double x2,double y2, double x3, double y3){ // x3,y3 is the point
+	/**
+	 * Determine distance between the point and the line
+	 * @param x1 x1 of line
+	 * @param y1 y1 of line
+	 * @param x2 x2 of line
+	 * @param y2 y2 of line
+	 * @param x3 x of point
+	 * @param y3 y of point
+	 * @return Distance
+	 */
+	double pointLineDist(double x1, double y1, double x2,double y2, double x3, double y3){
+		double px = x2-x1;
+		double py = y2-y1;
 		
-    double px = x2-x1;
-    double py = y2-y1;
-
-    double cross = px*px + py*py;
-
-    double u =  ((x3 - x1) * px + (y3 - y1) * py) / cross;
-
-    if( u > 1){
-        u = 1;
-    }
-    else if( u < 0){
-        u = 0;
-    }
-
-    double x = x1 + u * px;
-    double y = y1 + u * py;
-
-    double dx = x - x3;
-    double dy = y - y3;
-
-    // Note: If the actual distance does not matter,
-    // if you only want to compare what this function;
-    // returns to other results of this function, you;
-    // can just return the squared distance instead;
-    // (i.e. remove the sqrt) to gain a little performance;
-
-    double dist = Math.sqrt(dx*dx + dy*dy);
-
-    return dist;
+		double dot = px*px + py*py;
+		
+		double u =  ((x3 - x1) * px + (y3 - y1) * py) / dot;
+		
+		if( u > 1)
+			u = 1;
+		else if( u < 0)
+			u = 0;
+		
+		double x = x1 + u * px;
+		double y = y1 + u * py;
+		
+		double dx = x - x3;
+		double dy = y - y3;
+		
+		double dist = Math.sqrt(dx*dx + dy*dy);
+		
+		return dist;
 	}
 	
-	
+	/**
+	 * Detects intersection of a circle and a line
+	 * @param a hitbox of circle
+	 * @param b hitbox of line
+	 * @return Whether circle intersects with line
+	 */
 	boolean CLIntersect(CircleHit a, Line b){
 		
-		
-		double distance = dist(b.getX1(), b.getY1(), b.getX2(), b.getY2(), a.getX(), a.getY());
+		// distance between line and center of circle
+		double distance = pointLineDist(b.getX1(), b.getY1(), b.getX2(), b.getY2(), a.getX(), a.getY());
 		
 		if (distance < a.getR()){
 			return true;
 		}
 		
 		return false;
-		
-	}
-	private boolean LLIntersect(Line a, Line b){
-		return false;
-		
 	}
 
 }
