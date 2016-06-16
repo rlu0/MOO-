f927041948a43715c1c1af58b317496b247a8abe
Michael <Michael@ElMico>
2016-06-16 12:17:59 -0400

    added header comments

--- src/Serverito.java
+++ src/Serverito.java
@@ -14,7 +14,12 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-
+/**
+ * Sever class that hosts the game and communicates with players 
+ * @author Michael, Ray, Tony
+ * @version June 16, 2016
+ *
+ */
 public class Serverito
 {
 	ServerSocket serverSock;// server socket for connection
